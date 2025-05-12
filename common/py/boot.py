207c35430bc7266927ed40b55e1237b2ddeba864
Bobby Christopher <b.christopher.3rd@gmail.com>
2025-05-12 15:43:43 -0400

    switched it up a bit

--- common/py/boot.py
+++ common/py/boot.py
@@ -5,7 +5,7 @@ from agentuity import autostart
 
 if __name__ == "__main__":
     # Check if AGENTUITY_API_KEY is set
-    if not os.environ.get("AGENTUITY_API_KEY") or not os.environ.get("AGENTUITY_SDK_KEY"):
+    if not os.environ.get("AGENTUITY_API_KEY") and not os.environ.get("AGENTUITY_SDK_KEY"):
         print(
             "\033[31m[ERROR] AGENTUITY_API_KEY or AGENTUITY_SDK_KEY is not set. This should have been set automatically by the Agentuity CLI or picked up from the .env file.\033[0m"
         )
