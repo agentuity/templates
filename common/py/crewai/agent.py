4e088d9e64dce7dd8778b0e2739baa157bcc502b
Jason Walkow <jason.walkow@gmail.com>
2025-08-07 15:39:49 -0600

    rename to agentuity_agents

--- common/py/crewai/agent.py
+++ common/py/crewai/agent.py
@@ -1,12 +1,4 @@
 from agentuity import AgentRequest, AgentResponse, AgentContext
-import os
-import sys
-
-# Add the current directory to Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.insert(0, current_dir)
-
 from crew import MyCrew
 
 def welcome():
